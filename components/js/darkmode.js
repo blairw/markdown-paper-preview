289f0a339e9a3947e0c8971920d7590cf4105c8b
Blair Wang <hello@blair.wang>
2022-05-23 13:10:52 +1000

    [feat] Completed Dark Mode

--- components/js/darkmode.js
+++ components/js/darkmode.js
@@ -1,16 +1,26 @@
+var globalCurrentDarkMode = false;
+
 function setDarkModeAccordingToBrowser() {
 	// Adapted from https://stackoverflow.com/questions/50840168/how-to-detect-if-the-os-is-in-dark-mode-in-browsers
 	if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-		setDarkMode(true);
+		globalCurrentDarkMode = true;
 	} else {
-		setDarkMode(false);
+		globalCurrentDarkMode = false;
+	}
+	setDarkMode();
+}
+
+function setDarkMode() {
+	if (globalCurrentDarkMode) {
+		$("body").addClass("isDarkMode");
+		$("body").removeClass("isLightMode");
+	} else {
+		$("body").removeClass("isDarkMode");
+		$("body").addClass("isLightMode");
 	}
 }
 
-function setDarkMode(isDarkMode) {
-	if (isDarkMode) {
-		
-	} else {
-		
-	}
+function toggleDarkMode() {
+	globalCurrentDarkMode = !globalCurrentDarkMode;
+	setDarkMode();
 }
--- example/generate.sh
+++ example/generate.sh
@@ -8,23 +8,25 @@ echo "${RED}Now running: generate.sh${NC}"
 OUTPUT_FILE="./paper.html"
 
 # Begin generating
-cat ../components/01-header.html > $OUTPUT_FILE
+cat ../components/html/01-header.html > $OUTPUT_FILE
 
 # Embed CSS
 echo "<style>" >> $OUTPUT_FILE
-cat ../components/*.css >> $OUTPUT_FILE
+cat ../components/css/*.css >> $OUTPUT_FILE
 echo "</style>" >> $OUTPUT_FILE
 
 echo "<script>" >> $OUTPUT_FILE
 cat ../node_modules/jquery/dist/jquery.min.js >> $OUTPUT_FILE
+echo >> $OUTPUT_FILE
+cat ../components/js/*.js >> $OUTPUT_FILE
 echo "</script>" >> $OUTPUT_FILE
 
 # Generate the rest
-cat ../components/20-header2.html >> $OUTPUT_FILE
+cat ../components/html/20-header2.html >> $OUTPUT_FILE
 pandoc paper.md --toc -s -o temp.md
 pandoc temp.md \
     >> $OUTPUT_FILE
-cat ../components/99-footer.html >> $OUTPUT_FILE
+cat ../components/html/99-footer.html >> $OUTPUT_FILE
 
 # Done
 echo "${GREEN}✔ Finished running: generate_html.sh${NC}"
